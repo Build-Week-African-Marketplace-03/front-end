8ce28b6c2dc3682906b560d367d4fa401b471955
Nelson Maldonado <play2nino@gmail.com>
2021-08-25 09:19:50 -0700

    added button styled component with 3 colors, working on nav

---
+++ african-marketplace/src/css/Button.js
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components"
+
+const ButtonStyle = styled.button`
+  padding: 6px 10px;
+  margin: 5px;
+  border: none;
+  border-radius: 3px;
+  color: white;
+
+  ${(props) => (props.type === "primary" ? "background: #deedd6" : null)}
+  ${(props) => (props.type === "secondary" ? "background: #7bed8d" : null)}
+  ${(props) => (props.type === "third" ? "background: #a6f" : null)}
+`
+
+export default ButtonStyle
