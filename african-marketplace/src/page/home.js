0b19aeb2af19d2aa40a14862cd43e361e9707f08
Nelson Maldonado <play2nino@gmail.com>
2021-08-25 08:16:38 -0700

    git working added homepage text

--- african-marketplace/src/page/home.js
+++ african-marketplace/src/page/home.js
@@ -1,15 +1,23 @@
-import React from "react";
-import FormLogin from "../component/FormLogin";
+import React from "react"
+import FormLogin from "../component/FormLogin"
 
 export default function HomePage(props) {
   return (
-    <div>
-      <h2>Home Page</h2>
-
+    <div classname="homeMainDiv">
+      <h2>
+        Anyone can start selling today - <br></br>it really is that simple
+      </h2>
+      <div></div>
       <div>
-        <p>Some home page stuff</p>
+        <p>African women are cut off from the global economy.</p>
+        <p>They have produce, but no opportunity to sell it.</p>
+        <p>
+          This site is an online market place that allows African women to
+          access customers around the world and get better prices for their
+          produce by cutting out middle men and connecting them directly with
+          buyers in Europe, America, Asia and Australia.
+        </p>
       </div>
-
     </div>
-  );
+  )
 }
