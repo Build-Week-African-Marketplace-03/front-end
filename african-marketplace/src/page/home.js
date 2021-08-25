8f5c66798e53be265af5a903612f2f6078871ba2
Nelson Maldonado <play2nino@gmail.com>
2021-08-25 16:51:59 -0700

    map added

--- african-marketplace/src/page/home.js
+++ african-marketplace/src/page/home.js
@@ -8,22 +8,25 @@ export default function HomePage(props) {
   return (
     <>
       <div className="homeMainDiv">
-        <div className="topLeftDiv ">
-          <h2>
-            Anyone can start selling today - <br></br>it really is that simple
-          </h2>
-        </div>
+        <div className="leftSide">
+          <div className="topLeftDiv ">
+            <h2>
+              Anyone can start selling today - <br></br>it really is that simple
+            </h2>
+          </div>
 
-        <div className="bottomLeftDiv">
-          <p>African women are cut off from the global economy.</p>
-          <p>They have produce, but no opportunity to sell it.</p>
-          <p>
-            This site is an online market place that allows African women to
-            access customers around the world and get better prices for their
-            produce by cutting out middle men and connecting them directly with
-            buyers in Europe, America, Asia and Australia.
-          </p>
+          <div className="bottomLeftDiv">
+            <p>African women are cut off from the global economy.</p>
+            <p>They have produce, but no opportunity to sell it.</p>
+            <p>
+              This site is an online market place that allows African women to
+              access customers around the world and get better prices for their
+              produce by cutting out middle men and connecting them directly
+              with buyers in Europe, America, Asia and Australia.
+            </p>
+          </div>
         </div>
+        <img src="/images/map.png" className="map"></img>
       </div>
     </>
   )
