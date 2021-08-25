702750279129de1f88568aefb70c0eaff5348a15
Nelson Maldonado <play2nino@gmail.com>
2021-08-25 16:20:20 -0700

    logo added to nav

--- african-marketplace/src/component/navigation-bar.js
+++ african-marketplace/src/component/navigation-bar.js
@@ -1,15 +1,22 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, Container } from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "../css/nav.css"
 
+console.log()
 export default function NavigationBar(props) {
   return (
     <Navbar>
       <Container>
-        <Navbar.Brand href="/">African Marketplace</Navbar.Brand>
+        <div>
+          <img src="/images/avo.png"></img>
+          <Navbar.Brand href="/" className="logoName">
+            AfriCart
+          </Navbar.Brand>
+        </div>
         <Nav.Link href="/product">Product</Nav.Link>
         <Nav.Link href="/inventory">Inventory</Nav.Link>
         <Nav.Link href="/profile">Profile</Nav.Link>
       </Container>
     </Navbar>
-  );
+  )
 }
