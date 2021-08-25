9a659ad728183462c8426f6d54ee529b252fa497
beefybroccoli <tedkimemail@yahoo.com>
2021-08-24 20:04:54 -0700

    (merge with main and tested)

--- .history/african-marketplace/src/App_20210824195757.js
+++ .history/african-marketplace/src/App_20210824195757.js
@@ -10,75 +10,82 @@ import ProfilePage from "./page/profile";
 import ContactPage from "./page/contact";
 import Specific_Product from "./page/specific_product";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import React, { useState, UseEffect, useEffect } from "react";
-import { reach } from "yup";
+import React, { useState, UseEffect, useEffect} from 'react'
+import { reach } from 'yup'
 import FormLogin from "./component/FormLogin";
 import FormProfile from "./component/FormProfile";
-import profileSchema from "./validation/profileFormSchema";
+import profileSchema from './validation/profileFormSchema'
 import loginSchema from "./validation/loginSchema";
 
 const initialProfileValues = {
-  sellerName: "",
-  username: "",
-  email: "",
-  phoneNumber: "",
-  address: "",
-  password: "",
-};
-const initialLoginValues = {
-  username: "",
-  password: "",
-};
+  sellerName: '',
+  username: '',
+  email: '',
+  phoneNumber: '',
+  address: '',
+  password: '',
+}
+const initialLoginValues={
+  username: '',
+  password: '',
+}
 const initialItemValues = {
-  productName: "",
+  productName: '',
   quantity: 0,
-  productDescription: "",
-  productCategory: "",
-  prodSubCategory: "",
-};
+  productDescription: '',
+  productCategory: '',
+  prodSubCategory:'',
+}
 const initialProfileErrors = {
-  sellerName: "",
-  username: "",
-  email: "",
-  phoneNumber: "",
-  address: "",
-  productName: "",
-  quantity: "",
-  productDescription: "",
-  productCategory: "",
-  prodSubCategory: "",
-};
+  sellerName: '',
+  username: '',
+  email: '',
+  phoneNumber: '',
+  address: '',
+  productName: '',
+  quantity: '',
+  productDescription: '',
+  productCategory: '',
+  prodSubCategory:'',
+}
 
-const initialDisabled = true;
+
+const initialDisabled = true
+
+<<<<<<< HEAD
+function App(props) {
+=======
 function App() {
-  const [profileValues, setProfileValues] = useState(initialProfileValues);
-  const [loginValues, setLoginValues] = useState(initialLoginValues);
-  const [itemValues, setItemValues] = useState(initialItemValues);
-  const [formErrors, setFormErrors] = useState(initialProfileErrors);
-  const [disabled, setDisabled] = useState(initialDisabled);
-
+  
+  const [profileValues, setProfileValues] = useState(initialProfileValues)
+  const [loginValues, setLoginValues] = useState(initialLoginValues)
+  const [itemValues, setItemValues] = useState(initialItemValues)
+  const [formErrors, setFormErrors] = useState(initialProfileErrors)
+  const [disabled, setDisabled] = useState(initialDisabled) 
+  
   const validate = (name, value) => {
     reach(profileSchema, name)
       .validate(value)
-      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
-      .catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }));
-  };
+      .then(() => setFormErrors({ ...formErrors, [name]: ''}))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  }
 
   const inputChange = (name, value) => {
-    validate(name, value);
+    validate(name, value)
     setProfileValues({
-      ...profileValues,
-      [name]: value,
-    });
+      ...profileValues, 
+      [name]: value
+    })
     setLoginValues({
       ...loginValues,
-      [name]: value,
-    });
+      [name]: value
+    })
     setItemValues({
       ...itemValues,
-      [name]: value,
-    });
-  };
+      [name]: value
+    })
+  }
+
 
   const profileSubmit = () => {
     const newProfile = {
@@ -88,19 +95,19 @@ function App() {
       phoneNumber: profileValues.phoneNumber.trim(),
       address: profileValues.address.trim(),
       password: profileValues.password.trim(),
-    };
-    console.log(newProfile);
-    setProfileValues(initialProfileValues);
-  };
-
+    }
+    console.log(newProfile)
+    setProfileValues(initialProfileValues)
+  }
+  
   const loginSubmit = () => {
     const login = {
       username: loginValues.username,
-      password: loginValues.password,
-    };
-    console.log(login);
-    setLoginValues(initialLoginValues);
-  };
+      password: loginValues.password
+    }
+    console.log(login)
+    setLoginValues(initialLoginValues)
+  }
 
   const itemSubmit = () => {
     const newItemListing = {
@@ -108,14 +115,16 @@ function App() {
       quantity: itemValues.quantity.trim(),
       productDescription: itemValues.productDescription,
       productCategory: itemValues.productCategory.trim(),
-      prodSubCategory: itemValues.productCategory.trim(),
-    };
-  };
+      prodSubCategory: itemValues.productCategory.trim()
+    }
+  }
 
   useEffect(() => {
-    profileSchema.isValid(profileValues).then((valid) => setDisabled(!valid));
-  }, [profileValues]);
+    profileSchema.isValid(profileValues).then(valid => setDisabled(!valid))
+  },[profileValues])
+  
 
+>>>>>>> 3db3b81334d306786f964717b6c4f4033077ab2c
   return (
     <div className="App">
       <header>
@@ -126,7 +135,7 @@ function App() {
           <Switch>
             <Route exact path="/">
               <HomePage />
-              <FormLogin
+              <FormLogin 
                 values={loginValues}
                 change={inputChange}
                 submit={loginSubmit}
@@ -141,7 +150,7 @@ function App() {
               <Inventory />
             </Route>
             <Route path="/profile">
-              <FormProfile
+              <FormProfile 
                 values={profileValues}
                 change={inputChange}
                 submit={profileSubmit}
