fd963641f251a0dcc79c201ed9cea3ecdec87395
Nelson Maldonado <play2nino@gmail.com>
2021-08-26 00:38:20 -0700

    changed names inside input text boxes

--- african-marketplace/src/component/FormProfile.js
+++ african-marketplace/src/component/FormProfile.js
@@ -6,7 +6,7 @@ import "../css/signup.css"
 const FormProfile = () => (
   <div className="signUpMainDiv">
     <div className="signUpCard">
-      <div className="signIn">Hello</div>
+      <div className="signIn">Sign In</div>
       <Formik
         initialValues={{
           sellerName: "",
@@ -27,68 +27,82 @@ const FormProfile = () => (
         {(props) => (
           <form className="flex-column" onSubmit={props.handleSubmit}>
             <label>
-              Name:
+              {/* Name: */}
               <input
                 value={props.values.sellerName}
                 onChange={props.handleChange}
                 name="sellerName"
                 type="text"
+                className="nameBox"
+                placeholder="Name"
               />
               <div>{props.errors.sellerName}</div>
             </label>
             <label>
-              Email:
+              {/* Email: */}
               <input
                 value={props.values.email}
                 onChange={props.handleChange}
                 name="email"
                 type="email"
+                className="nameBox"
+                placeholder="Email"
               />
               <div>{props.errors.email}</div>
             </label>
             <label>
-              Phone Number:
+              {/* Phone Number: */}
               <input
                 value={props.values.phoneNumber}
                 onChange={props.handleChange}
                 name="phoneNumber"
                 type="tel"
+                className="nameBox"
+                placeholder="Phone Number"
               />
               <div>{props.errors.phoneNumber}</div>
             </label>
             <label>
-              Address:
+              {/* Address: */}
               <input
                 value={props.values.address}
                 onChange={props.handleChange}
                 name="address"
                 type="address"
+                className="nameBox"
+                placeholder="Address"
               />
               <div>{props.errors.address}</div>
             </label>
             <label>
-              Seller ID:
+              {/* Seller ID: */}
               <input
                 value={props.values.username}
                 onChange={props.handleChange}
                 name="username"
                 type="text"
+                className="nameBox"
+                placeholder="Seller ID"
               />
               <div>{props.errors.username}</div>
             </label>
             <label>
-              Password
+              {/* Password */}
               <input
                 value={props.values.password}
                 onChange={props.handleChange}
                 name="password"
                 type="password"
+                className="nameBox"
+                placeholder="Password"
               />
               <div>{props.errors.password}</div>
             </label>
             <br />
             <div className="form-submit">
-              <button type="submit">Submit</button>
+              <button type="submit" className="submitSI">
+                Submit
+              </button>
             </div>
           </form>
         )}
