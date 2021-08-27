7856dea3db63681fbaee613f208b659b6b1e2ee5
Nelson Maldonado <play2nino@gmail.com>
2021-08-27 03:34:13 -0700

    about page

--- african-marketplace/src/page/about.js
+++ african-marketplace/src/page/about.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React from "react"
+import "../css/about.css"
 
 export default function AboutPage(props) {
   return (
-    <div>
-      <h2>About Page</h2>
+    <div className="aboutMainDiv">
+      <div className="aboutPage">
+        <h2>About Page</h2>
+      </div>
+      <div className="topBG"></div>
+
+      {/* <img className="imgBG" src="/images/about.png" alt="about"></img> */}
     </div>
-  );
+  )
 }
